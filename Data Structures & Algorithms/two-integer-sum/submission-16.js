51910866bb03be1320faed652fe790efc426bd0b
abhinyadesign <abhinyarajput@gmail.com>
2026-07-07 23:20:22 +0530

    Add: two-integer-sum - submission-16

---
+++ Data Structures & Algorithms/two-integer-sum/submission-16.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const hashMap = {};
+
+        for (let i = 0; i < nums.length; i++) {
+            const complement = target - nums[i];
+
+            if (hashMap.hasOwnProperty(complement)) {
+                return [hashMap[complement], i];
+            }
+
+            hashMap[nums[i]] = i;
+        }
+
+        return [];
+    }
+}
